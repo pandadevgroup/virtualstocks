8e7c7f44f766aa4a76ddfb68f2726aad43c368b0
Nathan Wang <nathan.r.wang@gmail.com>
2018-02-08 20:29:27 -0800

    add link to brand

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
 	selector: "vs-toolbar",
-	templateUrl: "toolbar.component.html"
+	templateUrl: "toolbar.component.html",
+	styleUrls: ["toolbar.component.scss"]
 })
 export class ToolbarComponent {}
