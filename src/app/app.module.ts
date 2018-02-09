9f823b7ce00adfc5fd3ccd35066552faaa944c7b
Nathan Wang <nathan.r.wang@gmail.com>
2018-02-08 18:56:39 -0800

    add basic app skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 
+import { MatToolbarModule } from "@angular/material";
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { ServiceWorkerModule } from '@angular/service-worker';
+
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from "./components/toolbar/toolbar.component";
 import { CoreModule } from '@app/core';
 
 import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+	AppComponent,
+	ToolbarComponent
   ],
   imports: [
 	BrowserModule,
 	BrowserAnimationsModule,
 	AppRoutingModule,
 	ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
-    CoreModule
+	CoreModule,
+
+	MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
