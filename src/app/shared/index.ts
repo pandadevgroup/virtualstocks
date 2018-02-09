c41ed424f9b267af25a9706082d9e12ad1e7af2f
Nathan Wang <nathan.r.wang@gmail.com>
2018-02-08 21:28:33 -0800

    add getStocks() function to UserService

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,2 +1,3 @@
 export * from "./shared.module";
 export * from "./services/user.service";
+export * from "./models/stock.model";
--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,6 +1,16 @@
 import { Injectable } from "@angular/core";
 
+import { Observable } from "rxjs/Observable";
+
+import { AngularFirestore } from "angularfire2/firestore";
+
+import { Stock } from "../models/stock.model";
+
 @Injectable()
 export class UserService {
+	constructor(private db: AngularFirestore) {}
 
+	getStocks(): Observable<Stock[]> {
+		return this.db.collection<Stock>("stocks").valueChanges();
+	}
 }
