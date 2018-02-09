f9f5062f1a83ced3f45bffccb466e3c70199a4d4
Nathan Wang <nathan.r.wang@gmail.com>
2018-02-08 21:23:05 -0800

    add shared module and user service stub

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,13 @@ import { NgModule, Optional, SkipSelf } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
 
+import { SharedModule } from "@app/shared";
+
 @NgModule({
 	imports: [
 		CommonModule,
 		HttpClientModule,
+		SharedModule
 	],
 	declarations: [],
 	providers: []
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule, ModuleWithProviders } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { UserService } from "./services/user.service";
+
+@NgModule({
+	imports: [CommonModule],
+	declarations: []
+})
+export class SharedModule {
+	static forRoot(): ModuleWithProviders {
+		return {
+			ngModule: SharedModule,
+			providers: [
+				UserService
+			]
+		};
+	}
+}
