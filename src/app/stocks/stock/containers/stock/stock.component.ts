125444837cf147e058d77844c1b6a873603527ae
Nathan Wang <nathan.r.wang@gmail.com>
2018-02-11 15:33:40 -0800

    add chart.js

--- src/app/stocks/stock/containers/stock/stock.component.ts
+++ src/app/stocks/stock/containers/stock/stock.component.ts
@@ -1,7 +1,14 @@
 import { Component } from "@angular/core";
 
+import * as Chart from "chart.js";
+
 @Component({
 	templateUrl: "stock.component.html",
 	styleUrls: ["stock.component.scss"]
 })
-export class StockComponent {}
+export class StockComponent {
+	ngOnInit() {
+		// Enter your code here I guess for testing
+		console.log(Chart);
+	}
+}
