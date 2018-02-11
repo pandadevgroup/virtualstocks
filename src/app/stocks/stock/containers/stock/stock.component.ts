ac8c26b237a8cbf8bd351bd4742bfe2860b8a0ae
Nathan Wang <nathan.r.wang@gmail.com>
2018-02-11 15:38:38 -0800

    get ticker from activatedroute

--- src/app/stocks/stock/containers/stock/stock.component.ts
+++ src/app/stocks/stock/containers/stock/stock.component.ts
@@ -1,4 +1,8 @@
 import { Component } from "@angular/core";
+import { ActivatedRoute, ParamMap } from "@angular/router";
+
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/switchMap";
 
 import * as Chart from "chart.js";
 
@@ -7,8 +11,18 @@ import * as Chart from "chart.js";
 	styleUrls: ["stock.component.scss"]
 })
 export class StockComponent {
+	stock$: Observable<any>;
+
+	constructor(private route: ActivatedRoute) {}
+
 	ngOnInit() {
 		// Enter your code here I guess for testing
 		console.log(Chart);
+
+		this.stock$ = this.route.paramMap
+			.switchMap((params: ParamMap) => {
+				const ticker = params.get("ticker");
+				return Observable.of({ ticker });
+			});
 	}
 }
