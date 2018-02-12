e452471a97c12636b16cd4e63d35debd564f47ca
Nathan Wang <nathan.r.wang@gmail.com>
2018-02-11 16:07:11 -0800

    add example chart

--- src/app/stocks/stock/containers/stock/stock.component.ts
+++ src/app/stocks/stock/containers/stock/stock.component.ts
@@ -6,40 +6,6 @@ import "rxjs/add/operator/switchMap";
 
 import * as Chart from "chart.js";
 
-
-var myLineChart = new Chart(document.getElementById("chart").getContext("2d"), {
-    type: 'line',
-    data: {
-      labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-        datasets: [{
-            label:"set 1",
-            borderColor: 'rgb(0, 122, 255)',
-            backgroundColor:'rgba(0, 122, 255, 0.5)',
-            data: [159, 163, 161, 155, 158, 157, 160],
-            borderWidth:2.5
-        }]
-    },
-    options: {
-      legend: {
-        display: false
-    },
-    tooltips: {
-        callbacks: {
-           label: function(tooltipItem) {
-                  return tooltipItem.yLabel;
-           }
-        }
-    },
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:false
-                }
-            }]
-        }
-    }
-});
-
 @Component({
 	templateUrl: "stock.component.html",
 	styleUrls: ["stock.component.scss"]
@@ -53,7 +19,39 @@ export class StockComponent {
 
 	ngOnInit() {
 		// Enter your code here I guess for testing
-		console.log(this.canvasEl.nativeElement);
+
+		var myLineChart = new Chart(this.canvasEl.nativeElement.getContext("2d"), {
+			type: 'line',
+			data: {
+			  labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+				datasets: [{
+					label:"set 1",
+					borderColor: 'rgb(0, 122, 255)',
+					backgroundColor:'rgba(0, 122, 255, 0.5)',
+					data: [159, 163, 161, 155, 158, 157, 160],
+					borderWidth:2.5
+				}]
+			},
+			options: {
+			  legend: {
+				display: false
+			},
+			tooltips: {
+				callbacks: {
+				   label: function(tooltipItem) {
+						  return tooltipItem.yLabel;
+				   }
+				}
+			},
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:false
+						}
+					}]
+				}
+			}
+		});
 
 		this.stock$ = this.route.paramMap
 			.switchMap((params: ParamMap) => {
