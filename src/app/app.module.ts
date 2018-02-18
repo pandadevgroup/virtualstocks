6f41903a39b2d7575d78fb8e93243b34e87457a8
Nathan Wang <nathan.r.wang@gmail.com>
2018-02-17 18:26:26 -0800

    refactor into core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,15 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 
-import { StoreModule } from "@ngrx/store";
-import { EffectsModule } from "@ngrx/effects";
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { StoreRouterConnectingModule, routerReducer } from '@ngrx/router-store';
-
-import { AngularFireModule } from "angularfire2";
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-
 import { MatToolbarModule } from "@angular/material";
-
-import { AppRoutingModule } from "./app-routing.module";
-
-import { ServiceWorkerModule } from "@angular/service-worker";
 
 import * as fromContainers from "./containers";
 import * as fromComponents from "./components";
 import { CoreModule } from "@app/core";
 import { AuthModule } from "@app/auth";
 import { StocksModule } from "@app/stocks";
+
+import { AppRoutingModule } from "./app-routing.module";
 
 import { environment } from "@env/environment";
 
@@ -34,22 +23,6 @@ import { environment } from "@env/environment";
 		BrowserModule,
 		BrowserAnimationsModule,
 		AppRoutingModule,
-		ServiceWorkerModule.register("/ngsw-worker.js", {
-			enabled: environment.production
-		}),
-
-		AngularFireModule.initializeApp(environment.firebase),
-		AngularFirestoreModule,
-		AngularFireAuthModule,
-
-		StoreModule.forRoot({
-			router: routerReducer
-		}),
-		EffectsModule.forRoot([]),
-		environment.production ? [] : StoreDevtoolsModule.instrument(),
-		StoreRouterConnectingModule.forRoot({
-			stateKey: 'router'
-		}),
 
 		CoreModule,
 		AuthModule,
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,40 @@ import { NgModule, Optional, SkipSelf } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
 
+import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule, routerReducer } from '@ngrx/router-store';
+
+import { AngularFireModule } from "angularfire2";
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { ServiceWorkerModule } from "@angular/service-worker";
+
 import { services } from "../services";
 
 @NgModule({
 	imports: [
 		CommonModule,
-		HttpClientModule
+		HttpClientModule,
+
+		ServiceWorkerModule.register("/ngsw-worker.js", {
+			enabled: environment.production
+		}),
+
+		AngularFireModule.initializeApp(environment.firebase),
+		AngularFirestoreModule,
+		AngularFireAuthModule,
+
+		StoreModule.forRoot({
+			router: routerReducer
+		}),
+		EffectsModule.forRoot([]),
+		environment.production ? [] : StoreDevtoolsModule.instrument(),
+		StoreRouterConnectingModule.forRoot({
+			stateKey: 'router'
+		}),
 	],
 	declarations: [],
 	providers: [
