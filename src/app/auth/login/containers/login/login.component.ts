32156ef3d08ee3998cdf4069aafac151048fbf71
Nathan Wang <nathan.r.wang@gmail.com>
2018-02-19 00:13:12 -0800

    redirect from login component

--- src/app/auth/login/containers/login/login.component.ts
+++ src/app/auth/login/containers/login/login.component.ts
@@ -1,15 +1,39 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 
 import { Store } from "@ngrx/store";
 
+import * as fromRoot from "@app/core/store";
 import * as fromAuth from "@app/auth/store";
+
+import { Subscription } from "rxjs/Subscription";
+import { filter, take } from "rxjs/operators";
 
 @Component({
 	templateUrl: "login.component.html",
 	styleUrls: ["login.component.scss"]
 })
-export class LoginComponent {
-	constructor(private store: Store<fromAuth.AuthState>) {}
+export class LoginComponent implements OnInit, OnDestroy {
+	subscription: Subscription;
+
+	constructor(private store: Store<fromRoot.State>) {}
+
+	ngOnInit() {
+		this.subscription = this.store
+			.select(fromAuth.getUserLoggedIn)
+			.pipe(
+				filter(loggedIn => loggedIn),
+				take(1)
+			)
+			.subscribe(loggedIn => {
+				this.store.dispatch(new fromRoot.Go({
+					path: ["/home"]
+				}));
+			});
+	}
+
+	ngOnDestroy() {
+		this.subscription.unsubscribe();
+	}
 
 	login() {
 		this.store.dispatch(new fromAuth.Login({
