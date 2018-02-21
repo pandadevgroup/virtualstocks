2ec88f6ff7139c201347fc04a9609f02ded64f20
Nathan Wang <nathan.r.wang@gmail.com>
2018-02-20 23:19:27 -0800

    add stock type

--- src/app/stocks/models/order.model.ts
+++ src/app/stocks/models/order.model.ts
@@ -3,10 +3,19 @@ export interface StockOrder {
 	ticker: string;
 	quantity: number;
 	fulfilled: boolean;
+	error: string | null;
+	type: StockOrderType;
 }
 
 export interface BuyStockOrder {
 	uid: string;
 	ticker: string;
 	quantity: number;
+}
+
+export class StockOrderType {
+	static BUY: "buy";
+	static LIMIT: "limit";
+	static SHORT: "short";
+	static SELL: "sell";
 }
--- src/app/stocks/store/effects/orders.effects.ts
+++ src/app/stocks/store/effects/orders.effects.ts
@@ -6,7 +6,7 @@ import { switchMap, map, catchError, take } from "rxjs/operators";
 
 import * as fromActions from "../actions";
 import { OrdersService } from "@app/stocks/services";
-import { BuyStockOrder } from "@app/stocks/models";
+import { BuyStockOrder, StockOrderType } from "@app/stocks/models";
 
 @Injectable()
 export class OrdersEffects {
@@ -18,7 +18,12 @@ export class OrdersEffects {
 	@Effect()
 	buyStock$ = this.actions$.ofType(fromActions.BUY_STOCK).pipe(
 		map((action: fromActions.BuyStock) => action.payload),
-		map((order: BuyStockOrder) => ({ ...order, fulfilled: false })),
+		map((order: BuyStockOrder) => ({
+			...order,
+			fulfilled: false,
+			error: null,
+			type: StockOrderType.BUY
+		})),
 		switchMap(order => this.ordersService.buyStock(order).pipe(
 			map(() => new fromActions.BuyStockSuccess()),
 			catchError(error => of(new fromActions.BuyStockFail(error)))
