04e8928ef2328a6694417e22a9c90dc28d66baab
Nathan Wang <nathan.r.wang@gmail.com>
2018-02-21 14:19:02 -0800

    clear error on CLEAR_STOCK_DETAIL action

--- src/app/stocks/store/reducers/stocks-data.reducer.ts
+++ src/app/stocks/store/reducers/stocks-data.reducer.ts
@@ -45,6 +45,7 @@ export function reducer(state = initialState, action: fromStocks.StocksDataActio
 		case fromStocks.CLEAR_STOCK_DETAIL: {
 			return {
 				...state,
+				error: null,
 				stockDetail: null
 			};
 		}
