d5e7399d0a687f562d157646bda87f326a4e9576
Nathan Wang <nathan.r.wang@gmail.com>
2018-02-21 23:22:31 -0800

    set transaction dialog title

--- src/app/stocks/stock/components/stock-transaction/stock-transaction.dialog.ts
+++ src/app/stocks/stock/components/stock-transaction/stock-transaction.dialog.ts
@@ -11,4 +11,14 @@ export class StockTransactionDialog {
 	constructor(
 		@Inject(MAT_DIALOG_DATA) private data: { stock: Stock, type: StockOrderType }
 	) {}
+
+	get title(): string {
+		let action;
+		if (this.data.type === StockOrderType.BUY) action = "Buy";
+		else if (this.data.type === StockOrderType.SELL) action = "Sell";
+		else if (this.data.type === StockOrderType.LIMIT) action = "Limit";
+		else if (this.data.type === StockOrderType.SHORT) action = "Short";
+
+		return `${action} ${this.data.stock.ticker}`;
+	}
 }
