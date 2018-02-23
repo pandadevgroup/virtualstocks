35bf839e5c1fdd4720ad409a6f60c7876e8fbbe8
Nathan Wang <nathan.r.wang@gmail.com>
2018-02-22 17:37:48 -0800

    update transactions reducer

--- src/app/stocks/store/reducers/transactions.reducer.ts
+++ src/app/stocks/store/reducers/transactions.reducer.ts
@@ -1,18 +1,24 @@
 import * as fromTransactions from "../actions/transactions.actions";
+import { StockTransaction } from "@app/stocks/models";
 
 export interface TransactionsState {
 	loading: boolean;
+	loaded: boolean;
+	transactions: StockTransaction[];
 	error: any;
 }
 
 export const initialState: TransactionsState = {
 	loading: false,
+	loaded: false,
+	transactions: [],
 	error: null
 };
 
 export function reducer(state = initialState, action: fromTransactions.OrdersAction): TransactionsState {
 	switch(action.type) {
-		case fromTransactions.STOCK_TRANSACTION: {
+		case fromTransactions.STOCK_TRANSACTION:
+		case fromTransactions.LOAD_TRANSACTIONS: {
 			return {
 				...state,
 				loading: true
@@ -25,7 +31,16 @@ export function reducer(state = initialState, action: fromTransactions.OrdersAct
 				error: null
 			};
 		}
-		case fromTransactions.STOCK_TRANSACTION_FAIL: {
+		case fromTransactions.LOAD_TRANSACTIONS_SUCCESS: {
+			return {
+				...state,
+				loading: false,
+				error: null,
+				transactions: action.payload
+			}
+		}
+		case fromTransactions.STOCK_TRANSACTION_FAIL:
+		case fromTransactions.LOAD_TRANSACTIONS_FAIL: {
 			return {
 				...state,
 				loading: false,
@@ -38,3 +53,5 @@ export function reducer(state = initialState, action: fromTransactions.OrdersAct
 
 export const getTransactionsLoading = (state: TransactionsState) => state.loading;
 export const getTransactionsError = (state: TransactionsState) => state.error;
+export const getTransactions = (state: TransactionsState) => state.transactions;
+export const getTransactionsLoaded = (state: TransactionsState) => state.loaded;
