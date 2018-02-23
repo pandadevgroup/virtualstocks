89303725366fe19c4be6e1a3f8cacb7545927009
Nathan Wang <nathan.r.wang@gmail.com>
2018-02-23 12:31:44 -0800

    expand loginWithGoogle$ test

--- src/app/auth/store/effects/user.effects.spec.ts
+++ src/app/auth/store/effects/user.effects.spec.ts
@@ -8,6 +8,7 @@ import { Actions } from "@ngrx/effects";
 import { provideMockActions } from "@ngrx/effects/testing";
 
 import { Observable } from "rxjs/Observable";
+import "rxjs/add/observable/throw";
 import { of } from "rxjs/observable/of";
 import { ReplaySubject } from "rxjs/ReplaySubject";
 
@@ -23,7 +24,7 @@ import { environment } from "@env/environment";
 describe("User Effects", () => {
 	let effects: UserEffects;
 	let actions$: Observable<any>;
-	let service: AuthService;
+	let service;
 	let logoutSpy;
 
 	beforeEach(() => {
@@ -52,6 +53,18 @@ describe("User Effects", () => {
 		it("should dispatch a LoginSuccess action", () => {
 			const action = new fromActions.LoginWithGoogle();
 			const completion = new fromActions.LoginSuccess(mockUser);
+
+			actions$ = hot("-a", { a: action });
+			const expected = cold("-b", { b: completion });
+
+			expect(effects.loginWithGoogle$).toBeObservable(expected);
+		});
+
+		it("should dispatch a LoginFailure action on fail", () => {
+			service.loginWithGoogle.and.returnValue(Observable.throw("Fake Error"));
+
+			const action = new fromActions.LoginWithGoogle();
+			const completion = new fromActions.LoginFailure("Fake Error");
 
 			actions$ = hot("-a", { a: action });
 			const expected = cold("-b", { b: completion });
