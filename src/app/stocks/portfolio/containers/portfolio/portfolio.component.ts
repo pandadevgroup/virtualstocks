bf7b93b702bce89bec85e4b24ef6f6e85634ab59
Nathan Wang <nathan.r.wang@gmail.com>
2018-02-23 18:30:45 -0800

    update portfolio component

--- src/app/stocks/portfolio/containers/portfolio/portfolio.component.ts
+++ src/app/stocks/portfolio/containers/portfolio/portfolio.component.ts
@@ -1,7 +1,21 @@
 import { Component } from "@angular/core";
 
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs/Observable";
+
+import * as fromStocks from "@app/stocks/store";
+import { Portfolio } from "@app/stocks/models";
+
 @Component({
 	templateUrl: "portfolio.component.html",
 	styleUrls: ["portfolio.component.scss"]
 })
-export class PortfolioComponent {}
+export class PortfolioComponent {
+	portfolio$: Observable<Portfolio>;
+
+	constructor(private store: Store<fromStocks.StocksState>) {}
+
+	ngOnInit() {
+		this.portfolio$ = this.store.select(fromStocks.getPortfolio);
+	}
+}
