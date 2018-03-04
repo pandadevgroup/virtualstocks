e4f497f953444de8ed91ced7eec724ee1db9b54f
Nathan Wang <nathan.r.wang@gmail.com>
2018-03-03 21:34:32 -0800

    add auth form stub

--- src/app/auth/components/index.ts
+++ src/app/auth/components/index.ts
@@ -1,5 +1,7 @@
 import { AuthButtonComponent } from "./auth-button/auth-button.component";
+import { AuthFormComponent } from "./auth-form/auth-form.component";
 
-export const components: any[] = [ AuthButtonComponent ];
+export const components: any[] = [ AuthButtonComponent, AuthFormComponent ];
 
 export * from "./auth-button/auth-button.component";
+export * from "./auth-form/auth-form.component";
