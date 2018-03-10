c1ba511d76cb62f20011ee6ebf54290f34f43f80
Nathan Wang <nathan.r.wang@gmail.com>
2018-03-10 13:41:28 -0800

    fix logout bug

--- src/app/auth/containers/logout/logout.component.ts
+++ src/app/auth/containers/logout/logout.component.ts
@@ -4,14 +4,16 @@ import { Store } from "@ngrx/store";
 import * as fromRoot from "@app/core/store";
 import * as fromAuth from "@app/auth/store";
 
+import { AuthService } from "@app/auth/services";
+
 @Component({
 	template: ""
 })
 export class LogoutComponent {
-	constructor(private store: Store<fromRoot.State>) {}
+	constructor(private store: Store<fromRoot.State>, private authService: AuthService) {}
 
 	ngOnInit() {
-		this.store.dispatch(new fromAuth.Logout());
+		if (this.authService.userId) this.store.dispatch(new fromAuth.Logout());
 		this.store.dispatch(new fromRoot.Go({
 			path: ["/"],
 			extras: { replaceUrl: true }
