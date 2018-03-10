22bd088af21e93d5ef8b2863d38988603ba3d0f5
Nathan Wang <nathan.r.wang@gmail.com>
2018-03-09 23:51:32 -0800

    add register typescript stub

--- src/app/auth/containers/register/register.component.ts
+++ src/app/auth/containers/register/register.component.ts
@@ -1,12 +1,27 @@
 import { Component } from "@angular/core";
 
+import { Store } from "@ngrx/store";
+
+import * as fromRoot from "@app/core/store";
+import * as fromAuth from "@app/auth/store";
+
 @Component({
 	templateUrl: "register.component.html",
 	styleUrls: ["register.component.scss", "../auth.styles.scss"]
 })
 export class RegisterComponent {
+	error$ = this.store.select(fromAuth.getUserError);
 
-	login() {
+	constructor(
+		private store: Store<fromRoot.State>,
+		private userEffects: fromAuth.UserEffects
+	) {}
+
+	loginWithGoogle() {
+		this.store.dispatch(new fromAuth.LoginWithGoogle());
+	}
+
+	register({ name, email, password }) {
 
 	}
 }
