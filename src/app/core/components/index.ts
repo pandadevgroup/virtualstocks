8cbc0a2eb3ae80420635bb3492c340a88c954f03
Nathan Wang <nathan.r.wang@gmail.com>
2018-03-11 11:29:09 -0700

    add sidebar stub

--- src/app/core/components/index.ts
+++ src/app/core/components/index.ts
@@ -1,5 +1,7 @@
 import { ToolbarComponent } from "./toolbar/toolbar.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
 
-export const components: any[] = [ ToolbarComponent ];
+export const components: any[] = [ ToolbarComponent, SidebarComponent ];
 
 export * from "./toolbar/toolbar.component";
+export * from "./sidebar/sidebar.component";
---
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "vs-sidebar",
+	templateUrl: "sidebar.component.html",
+	styleUrls: ["sidebar.component.scss"]
+})
+export class SidebarComponent {}
