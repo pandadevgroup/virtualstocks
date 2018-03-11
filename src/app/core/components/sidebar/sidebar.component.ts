d9d8d4c320bcdaf58557808851f6d9ffd25da583
Nathan Wang <nathan.r.wang@gmail.com>
2018-03-11 11:59:43 -0700

    add login logout to sidebar

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -1,8 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
 	selector: "vs-sidebar",
 	templateUrl: "sidebar.component.html",
 	styleUrls: ["sidebar.component.scss"]
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+	@Input() loggedIn: boolean;
+}
