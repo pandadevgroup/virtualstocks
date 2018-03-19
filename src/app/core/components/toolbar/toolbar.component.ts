1d4d23f2672dc219bf35eff8083b8a5d674b0f9b
Nathan Wang <nathan.r.wang@gmail.com>
2018-03-18 21:01:15 -0700

    add keyboard events to toolbar

--- src/app/core/components/toolbar/toolbar.component.ts
+++ src/app/core/components/toolbar/toolbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, ChangeDetectionStrategy, ViewChild, ElementRef, EventEmitter, OnInit, OnDestroy } from "@angular/core";
 import { FormControl, FormGroup } from "@angular/forms";
+import { DOWN_ARROW, ENTER, ESCAPE, UP_ARROW, TAB } from '@angular/cdk/keycodes';
 
 import { debounceTime, tap } from "rxjs/operators";
 import { Subject } from "rxjs/Subject";
@@ -24,7 +25,8 @@ export class ToolbarComponent implements OnInit, OnDestroy {
 	get searchResults() {
 		return this._searchResults;
 	}
-	@Input() set searchResults(searchResults: StockSearchResult[]) {
+	@Input()
+	set searchResults(searchResults: StockSearchResult[]) {
 		this._searchResults = searchResults;
 		this.searchResultSelectedIndex = 0;
 	}
@@ -40,20 +42,24 @@ export class ToolbarComponent implements OnInit, OnDestroy {
 	}
 
 	ngOnInit() {
-		this.searchForm.get("ticker").valueChanges.pipe(
-			debounceTime(200)
-		).takeUntil(this.ngUnsubscribe).subscribe((search: string) => {
-			search = search.trim();
-			this.partialTickerSearch.emit(search.toUpperCase());
-		});
+		this.searchForm
+			.get("ticker")
+			.valueChanges.pipe(debounceTime(200))
+			.takeUntil(this.ngUnsubscribe)
+			.subscribe((search: string) => {
+				search = search.trim();
+				this.partialTickerSearch.emit(search.toUpperCase());
+			});
 	}
 
 	onSubmit(el) {
 		if (this.ticker && this.ticker.trim() !== "") {
-			let success = this.searchResults.length > this.searchResultSelectedIndex;
-			this.tickerSearch.emit(success
-				? this.searchResults[this.searchResultSelectedIndex].symbol
-				: this.ticker.trim().toUpperCase()
+			let success =
+				this.searchResults.length > this.searchResultSelectedIndex;
+			this.tickerSearch.emit(
+				success
+					? this.searchResults[this.searchResultSelectedIndex].symbol
+					: this.ticker.trim().toUpperCase()
 			);
 		}
 		this.closeSearch(el);
@@ -73,6 +79,31 @@ export class ToolbarComponent implements OnInit, OnDestroy {
 	clearSearch() {
 		this.searchForm.get("ticker").setValue("");
 		this.searchResults = [];
+	}
+
+	handleKeydown(event: KeyboardEvent, el: any) {
+		const keyCode = event.keyCode;
+
+		// Prevent the default action on all escape key presses. This is here primarily to bring IE
+		// in line with other browsers. By default, pressing escape on IE will cause it to revert
+		// the input value to the one that it had on focus, however it won't dispatch any events
+		// which means that the model value will be out of sync with the view.
+		if (keyCode === ESCAPE) {
+			event.preventDefault();
+		}
+
+		// Close when pressing ESCAPE or ALT + UP_ARROW, based on the a11y guidelines.
+		// See: https://www.w3.org/TR/wai-aria-practices-1.1/#textbox-keyboard-interaction
+		if (keyCode === ESCAPE || (keyCode === UP_ARROW && event.altKey)) {
+			this.closeSearch(el);
+			event.stopPropagation();
+		} else if (keyCode === DOWN_ARROW) {
+			this.searchResultSelectedIndex++;
+			if (this.searchResultSelectedIndex >= this.searchResults.length) this.searchResultSelectedIndex = 0;
+		} else if (keyCode === UP_ARROW) {
+			this.searchResultSelectedIndex--;
+			if (this.searchResultSelectedIndex < 0) this.searchResultSelectedIndex = this.searchResults.length - 1;
+		}
 	}
 
 	onMenuClick() {
