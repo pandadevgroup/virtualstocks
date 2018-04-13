c630a393fe26ea9136828247fa2f48012e202df8
Nathan Wang <nathan.r.wang@gmail.com>
2018-04-12 17:39:33 -0700

    update home page

--- src/app/home/containers/home/home.component.ts
+++ src/app/home/containers/home/home.component.ts
@@ -2,12 +2,14 @@ import { Component } from "@angular/core";
 
 import { Observable } from "rxjs/Observable";
 
-import { Stock, PortfolioStock } from "@app/stocks";
+import { Stock, PortfolioStock, BatchStockData } from "@app/stocks";
 
 import { Store } from "@ngrx/store";
 import * as fromRoot from "@app/core/store";
 import * as fromStocks from "@app/stocks/store";
 import * as fromPortfolio from "@app/stocks/store/reducers/portfolio.reducer";
+
+import { tap } from "rxjs/operators";
 
 @Component({
 	templateUrl: "home.component.html",
@@ -16,11 +18,18 @@ import * as fromPortfolio from "@app/stocks/store/reducers/portfolio.reducer";
 export class HomeComponent {
 	portfolio$: Observable<fromPortfolio.PortfolioState>;
 	stocks$: Observable<PortfolioStock[]>;
+	stockPrices$: Observable<BatchStockData>;
 
 	constructor(private store: Store<fromRoot.State>) {}
 
 	ngOnInit() {
 		this.portfolio$ = this.store.select(fromStocks.getPortfolioState);
-		this.stocks$ = this.store.select(fromStocks.getAllPortfolioStocks);
+		this.stocks$ = this.store.select(fromStocks.getAllPortfolioStocks).pipe(
+			tap(stocks => {
+				const stockTickers = stocks.map(stock => stock.ticker);
+				this.store.dispatch(new fromStocks.QueryBatchStockPrices(stockTickers));
+			})
+		);
+		this.stockPrices$ = this.store.select(fromStocks.getBatchStocksData);
 	}
 }
